28ac20120a7864d7f5baf352bcfd20df2df65dcc
Andreas Jurat <andreasjurat@Andreass-MacBook-Pro-2.local>
2017-11-06 21:09:45 +0100

    added angular tutorial demo-app

--- demo-app/src/main.ts
+++ demo-app/src/main.ts
@@ -1,3 +1,4 @@
+import 'style-loader!bootstrap/dist/css/bootstrap.css';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
