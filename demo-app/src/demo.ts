0270027f58f117a642d3e5b0e93554a20f2435c9
Andreas Jurat <andreasjurat@Andreass-MacBook-Pro-2.local>
2017-11-08 23:29:20 +0100

    import flight object

--- demo-app/src/demo.ts
+++ demo-app/src/demo.ts
@@ -1,1 +1,13 @@
-console.debug('Hallo Welt!');
+import {Flight} from 'flight';
+
+let f: Flight{
+    id: 1,
+    from: 'Stuttgart',
+    to: 'Berlin Tegel',
+    date: '2017-11-16T17:00:00.00+01:00'
+}
+f.from = 'STR';
+f.to = 'TGX';
+console.debug('from',f.from);
+console.debug('flight',f);
+
