95629d79b0aebb583c67773283d54031f9608211
Andreas Jurat <andreasjurat@Andreass-MacBook-Pro-2.local>
2017-11-12 16:07:30 +0100

    Abstrakte Klassen und Sub-Klassen

---
+++ demo-app/src/adressen.ts
@@ -0,0 +1,21 @@
+export abstract class AbstractAddress{
+    id: number;
+    street: string;
+    zipCode: string;
+    city: string;
+
+    constructor(id: number){
+        this.id = id;
+    }
+    fullAddress(){
+        return this.street + "," + this.zipCode + " " + this.city;
+    }
+    abstract toCSV(): string;
+}
+
+export class CompanyAddress extends AbstractAddress{
+    companyName: string;
+    toCSV(){
+        return `${this.id};${this.companyName};${this.street};${this.zipCode};${this.city}`;
+    }
+}
